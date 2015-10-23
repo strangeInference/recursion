2cef1514e9cacaad54c8e319bff5a1136cf9cb06
Nikola Phipps <tesla.phipps@gmail.com>
2015-10-23 10:33:53 -0700

    still buggy

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';// your code goes here
+  if (typeof obj === "undefined"){
+    return '{}';
+  }
+  if (typeof obj === "string"){
+    return '"' + obj + '"';
+  }
+  if (typeof obj === "number"){
+    return String(obj);
+  }
+  if (obj === null){
+    return 'null';
+  }
+  if (typeof obj === "boolean"){
+    return String(obj);
+  }
+  if (Array.isArray(obj)){
+    var recArr = function(obj){
+      var arr = '';
+      arr += stringifyJSON(obj.shift());
+      if (obj.length){
+        arr += ',' + recArr(obj);
+      }
+      return arr;
+    };
+    return '[' + recArr(obj) + ']';
+  }
+  if (typeof obj === "object"){
+    for (var item in obj){
+      if (obj[item] !== undefined){
+        result += stringifyJSON(item) + ':' + stringifyJSON(obj[item]) + ',';
+      }
+    }
+    result = result.slice(0, result.length - 1);
+    result = '{' + result + '}'
+  }
+  return result
 };
+console.log(stringifyJSON({a: undefined}));

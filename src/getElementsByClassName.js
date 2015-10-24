1df87f15e7642603d1d5c1095ead72b6bfd190aa
Nikola Phipps <tesla.phipps@gmail.com>
2015-10-24 03:51:30 -0700

    first draft, fails specs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,6 +5,16 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
-){
-  // your code here
+){// your code here
+  var recursiveSeach = function(elements){
+    var results = [];
+    for (var item in elements.childNodes){
+      if (elements.childNodes[item].classList == className){
+        results.push(elements.childNodes[item]);
+      }
+      results.concat(recursiveSeach(elements.childNodes[item]));
+    }
+    return results;
+  };
+  return recursiveSeach(document.body);
 };
